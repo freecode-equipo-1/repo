979bfa4aae83f550b62e86d613ad682e4154cc30
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-02 16:35:02 -0400

    Pasar reportes a página

--- web/views.py
+++ web/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from datetime import timedelta
 
+from django.shortcuts import render
+from django.utils import timezone
+
+from web.models import ReporteInsumo
+
+def obtener_reportes_recientes():
+    return ReporteInsumo.objects.select_related("insumo").filter(
+        fecha_hora_reporte__gt=timezone.now() - timedelta(days=14)
+    )
 
 def inicio_view(request):
     plantilla = "inicio.html"
-    return render(request, plantilla)
+
+    reportes = obtener_reportes_recientes()
+    datos = {
+        "reportes": reportes,
+    }
+
+    return render(request, plantilla, datos)
