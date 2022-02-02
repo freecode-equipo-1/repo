26376b118794d5cdb51cd6364f96c01f4aad8c46
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-02 13:31:17 -0400

    Database en Heroku

--- freecode/settings.py
+++ freecode/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 PROJECT_DIR = Path(__file__).resolve().parent
 BASE_DIR = PROJECT_DIR.parent
@@ -25,6 +27,7 @@ BASE_DIR = PROJECT_DIR.parent
 SECRET_KEY = 'django-insecure-jq5q6y@%rg8k76z=*_k((qyl)3u$2(95=-!01lqx#p0f!*tm@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "local")
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -77,12 +80,18 @@ WSGI_APPLICATION = 'freecode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if ENVIRONMENT == "local":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+
 
 
 # Password validation
