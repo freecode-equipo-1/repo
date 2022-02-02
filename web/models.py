604de01589338a795f0638aee5c6752a73392946
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-02 14:07:21 -0400

    Agrega quién reporto cada insumo a DB

--- web/models.py
+++ web/models.py
@@ -45,6 +45,8 @@ class ReporteInsumo(models.Model):
     costo = models.DecimalField(max_digits=10, decimal_places=2)
     latitud = models.DecimalField(max_digits=10, decimal_places=7)
     longitud = models.DecimalField(max_digits=10, decimal_places=7)
+
+    reportado_por = models.ForeignKey("usuarios.Usuario", on_delete=models.SET_NULL, null=True)
     fecha_hora_reporte = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
