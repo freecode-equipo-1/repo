0095880047507cfd03348e2b48598302607a6523
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-03 12:23:40 -0400

    Geocodificación en backend

--- web/views.py
+++ web/views.py
@@ -1,15 +1,48 @@
+import random
 from datetime import timedelta
 
+import requests
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
 from web.models import Insumo, ReporteInsumo, TIPOS_INSUMO
 
+
 def obtener_reportes_recientes():
     return ReporteInsumo.objects.select_related("insumo").filter(
         fecha_hora_reporte__gt=timezone.now() - timedelta(days=14)
     )
+
+
+def geocodificar_direccion(direccion):
+    mapquest_key = settings.MAPQUESTAPI_ACCESS_KEY
+    url = f"http://www.mapquestapi.com/geocoding/v1/address?key={mapquest_key}&location={direccion}"
+
+    response = requests.get(url)
+
+    response_dict = response.json()
+    resultados = response_dict["results"]
+
+    if len(resultados) < 1:
+        #! Fallback: la API de Mapquest no es muy precisa en Latinoamerica,
+        #! y no hay muchas opciones gratuitas para el prototipo.
+        #! Si no se encuentra la ubicación, la agregamos a un punto aleatorio
+        #! en Caracas.
+
+        # (latitud, longitud)
+        puntos_en_caracas = [
+            (10.4806, -66.9036),
+        ]
+        return random.choice(puntos_en_caracas)
+
+    # En este punto, tenemos algún resultado y obtenemos sus coordenadas
+    primer_resultado = resultados[0]
+    return (
+        primer_resultado["latLng"]["lat"],
+        primer_resultado["latLng"]["lng"],
+    )
+
 
 
 def inicio_view(request):
