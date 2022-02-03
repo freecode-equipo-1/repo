00ef0e1fb2e9f99395e940b21550a4b6e497836c
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-03 10:35:50 -0400

    Ocultar búsqueda de tabla

--- static/js/reporte.js
+++ static/js/reporte.js
@@ -10,6 +10,7 @@ $(document).ready( function () {
     $('.datatable').DataTable({
         language: {
             url: "https://cdn.datatables.net/plug-ins/1.11.4/i18n/es_es.json"
-        }
+        },
+        bFilter: false,
     });
 } );
