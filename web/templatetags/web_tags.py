3c1e9a8a5e8de822aeda99e2c7dc17b646dbe4e3
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-03 12:46:57 -0400

    Arregla visualización de reportes

---
+++ web/templatetags/web_tags.py
@@ -0,0 +1,11 @@
+import locale
+
+from django import template
+
+locale.setlocale(locale.LC_ALL, '')
+register = template.Library()
+
+
+@register.filter()
+def currency(value):
+    return locale.currency(value, grouping=True)
