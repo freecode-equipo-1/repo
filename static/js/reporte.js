de3a724f6e688cc7a6ae0445d6f681c95063e4c2
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-02-03 10:33:57 -0400

    Datatable para mostrar resultados

--- static/js/reporte.js
+++ static/js/reporte.js
@@ -5,3 +5,11 @@ document.getElementById('reporte').addEventListener('click', () => {
 document.getElementById('close').addEventListener('click', () => {
     document.querySelector('.bg-modal').style.display = 'none';
 })
+
+$(document).ready( function () {
+    $('.datatable').DataTable({
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.11.4/i18n/es_es.json"
+        }
+    });
+} );
